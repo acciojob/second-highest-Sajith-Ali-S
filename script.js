efe44174c28555d040e7c96eeca0ebb8a37c9fe3
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 11:34:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+
+		let len = arr.length
+
+function isAllElementsSame (arr){
+    
+    for(let i =1;i<len; i++){
+        if(arr[0]!=arr[i])return false;
+    }
+    return true;
+}
+
+function secondHighest(arr) {
+    
+    if(arr.length==0 || isAllElementsSame(arr)==true)return -Infinity
+
+    let max = -Infinity;
+    for(let i =0;i<len;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+
+    let smax = -Infinity;
+    for(let j = 0 ; j < len ;  j++){
+
+        if(arr[j]>smax && arr[j]!=max){
+            smax=arr[j];
+        }
+
+    }
+return smax
+
+    
+}
+
